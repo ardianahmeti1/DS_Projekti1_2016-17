572265ff41400c1e5b965806774f63d29f4b9b9e
Ardian Ahmeti <ardiann83@gmail.com>
2017-04-30 19:19:07 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace DS_Projekti1_EnkriptimIFjalekalimit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKyqja = new System.Windows.Forms.Label();
+            this.btnKyqu = new System.Windows.Forms.Button();
+            this.txtTestFjalekalimi = new System.Windows.Forms.TextBox();
+            this.txtTestPerdoruesi = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmri = new System.Windows.Forms.TextBox();
+            this.btnRuaj = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRuajtjaFjalekalimi = new System.Windows.Forms.TextBox();
+            this.txtRuajtjaPerdoruesi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMbiemri = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblRuaj = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblKyqja
+            // 
+            this.lblKyqja.AutoSize = true;
+            this.lblKyqja.ForeColor = System.Drawing.Color.Red;
+            this.lblKyqja.Location = new System.Drawing.Point(244, 428);
+            this.lblKyqja.Name = "lblKyqja";
+            this.lblKyqja.Size = new System.Drawing.Size(0, 13);
+            this.lblKyqja.TabIndex = 55;
+            this.lblKyqja.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnKyqu
+            // 
+            this.btnKyqu.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKyqu.Location = new System.Drawing.Point(309, 455);
+            this.btnKyqu.Name = "btnKyqu";
+            this.btnKyqu.Size = new System.Drawing.Size(139, 54);
+            this.btnKyqu.TabIndex = 54;
+            this.btnKyqu.Text = "Kyçu";
+            this.btnKyqu.UseVisualStyleBackColor = true;
+            this.btnKyqu.Click += new System.EventHandler(this.btnKyqu_Click);
+            // 
+            // txtTestFjalekalimi
+            // 
+            this.txtTestFjalekalimi.Location = new System.Drawing.Point(364, 395);
+            this.txtTestFjalekalimi.Name = "txtTestFjalekalimi";
+            this.txtTestFjalekalimi.Size = new System.Drawing.Size(143, 20);
+            this.txtTestFjalekalimi.TabIndex = 53;
+            this.txtTestFjalekalimi.UseSystemPasswordChar = true;
+            // 
+            // txtTestPerdoruesi
+            // 
+            this.txtTestPerdoruesi.Location = new System.Drawing.Point(364, 353);
+            this.txtTestPerdoruesi.Name = "txtTestPerdoruesi";
+            this.txtTestPerdoruesi.Size = new System.Drawing.Size(143, 20);
+            this.txtTestPerdoruesi.TabIndex = 52;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(237, 398);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 18);
+            this.label8.TabIndex = 51;
+            this.label8.Text = "Fjalëkalimi:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(244, 356);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 18);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Përdoruesi:";
+            // 
+            // txtEmri
+            // 
+            this.txtEmri.Location = new System.Drawing.Point(276, 77);
+            this.txtEmri.Name = "txtEmri";
+            this.txtEmri.Size = new System.Drawing.Size(221, 20);
+            this.txtEmri.TabIndex = 40;
+            // 
+            // btnRuaj
+            // 
+            this.btnRuaj.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRuaj.Location = new System.Drawing.Point(283, 235);
+            this.btnRuaj.Name = "btnRuaj";
+            this.btnRuaj.Size = new System.Drawing.Size(165, 46);
+            this.btnRuaj.TabIndex = 48;
+            this.btnRuaj.Text = "Ruaj Fjalëkalimin";
+            this.btnRuaj.UseVisualStyleBackColor = true;
+            this.btnRuaj.Click += new System.EventHandler(this.btnRuaj_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(670, 24);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "RUAJTJA E FJALËKALIMIT TË ENKRIPTUAR NË DATABAZË";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(224, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Emri:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(183, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 18);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Fjalëkalimi:";
+            // 
+            // txtRuajtjaFjalekalimi
+            // 
+            this.txtRuajtjaFjalekalimi.Location = new System.Drawing.Point(276, 167);
+            this.txtRuajtjaFjalekalimi.Name = "txtRuajtjaFjalekalimi";
+            this.txtRuajtjaFjalekalimi.Size = new System.Drawing.Size(221, 20);
+            this.txtRuajtjaFjalekalimi.TabIndex = 43;
+            this.txtRuajtjaFjalekalimi.UseSystemPasswordChar = true;
+            // 
+            // txtRuajtjaPerdoruesi
+            // 
+            this.txtRuajtjaPerdoruesi.Location = new System.Drawing.Point(276, 137);
+            this.txtRuajtjaPerdoruesi.Name = "txtRuajtjaPerdoruesi";
+            this.txtRuajtjaPerdoruesi.Size = new System.Drawing.Size(221, 20);
+            this.txtRuajtjaPerdoruesi.TabIndex = 42;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(202, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 18);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Mbiemri:";
+            // 
+            // txtMbiemri
+            // 
+            this.txtMbiemri.Location = new System.Drawing.Point(276, 108);
+            this.txtMbiemri.Name = "txtMbiemri";
+            this.txtMbiemri.Size = new System.Drawing.Size(221, 20);
+            this.txtMbiemri.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(190, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 18);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Perdoruesi:";
+            // 
+            // lblRuaj
+            // 
+            this.lblRuaj.AutoSize = true;
+            this.lblRuaj.ForeColor = System.Drawing.Color.Red;
+            this.lblRuaj.Location = new System.Drawing.Point(270, 206);
+            this.lblRuaj.Name = "lblRuaj";
+            this.lblRuaj.Size = new System.Drawing.Size(0, 13);
+            this.lblRuaj.TabIndex = 49;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 521);
+            this.Controls.Add(this.lblKyqja);
+            this.Controls.Add(this.btnKyqu);
+            this.Controls.Add(this.txtTestFjalekalimi);
+            this.Controls.Add(this.txtTestPerdoruesi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblRuaj);
+            this.Controls.Add(this.txtEmri);
+            this.Controls.Add(this.btnRuaj);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRuajtjaFjalekalimi);
+            this.Controls.Add(this.txtRuajtjaPerdoruesi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMbiemri);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKyqja;
+        private System.Windows.Forms.Button btnKyqu;
+        private System.Windows.Forms.TextBox txtTestFjalekalimi;
+        private System.Windows.Forms.TextBox txtTestPerdoruesi;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEmri;
+        private System.Windows.Forms.Button btnRuaj;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRuajtjaFjalekalimi;
+        private System.Windows.Forms.TextBox txtRuajtjaPerdoruesi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMbiemri;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblRuaj;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace DS_Projekti1_EnkriptimIFjalekalimit
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public static string Salti()
+        {
+            char[] karakteret = new char[62];
+            karakteret =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+
+                data = new byte[12];
+                crypto.GetNonZeroBytes(data);
+            }
+            StringBuilder result = new StringBuilder(12);
+            foreach (byte b in data)
+            {
+                result.Append(karakteret[b % (karakteret.Length)]);
+            }
+            return result.ToString();
+        }
+
+        private void btnRuaj_Click(object sender, EventArgs e)
+        {
+            krijoProfil();
+        }
+
+        private void btnKyqu_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void krijoProfil()
+        {
+            if (txtEmri.Text != null && txtMbiemri.Text != null)
+            {
+                if (txtRuajtjaPerdoruesi.TextLength >= 6)
+                {
+                    if (txtRuajtjaFjalekalimi.TextLength >= 8 && txtRuajtjaFjalekalimi.TextLength <= 20)
+                    {
+                        
+                            try
+                            {
+                                string connectionString = "Server=127.0.0.1; Database=ds_projekti1; UserID=root; Password=805198;";
+
+                                MySqlConnection myDbConn = new MySqlConnection(connectionString);
+
+                                myDbConn.Open();
+
+                            /* Ketu ruhet fjalekalimi i enkriptuar 
+                             * mirepo meqenese ende nuk e kemi implementuar algoritmin vetem per test ja kam vene vleren 'test123'
+                            */
+                            string salti = Salti();
+                                string fjalekalimiEnkriptuar = Enkripto(txtRuajtjaFjalekalimi.Text, salti);
+
+                                string inserti = "insert into perdoruesit(username,emri,mbiemri,salt,fjalekalimi) values('" + txtRuajtjaPerdoruesi.Text + "','" + txtEmri.Text + "','" + txtMbiemri.Text + "','" + salti + "','" + fjalekalimiEnkriptuar + "')";
+
+                                MySqlCommand command = new MySqlCommand(inserti, myDbConn);
+
+
+
+
+
+                                command.ExecuteReader();
+                                lblRuaj.Text = " ";
+                                txtEmri.Text = "";
+                                txtMbiemri.Text = "";
+                                txtRuajtjaPerdoruesi.Text = "";
+                                txtRuajtjaFjalekalimi.Text = "";
+
+                                MessageBox.Show("Profili u ruajt me sukses");
+
+
+                                myDbConn.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                        
+                    }
+                    else
+                    {
+                        lblRuaj.Text = "Fjalekalimi nuk duhet te jete me i shkurter se 8 karaktere apo e i gjate se 20!";
+                    }
+                }
+                else
+                {
+                    lblRuaj.Text = "Perdoruesi duhet te kete 6 karaktere apo me shume!";
+                }
+            }
+            else
+            {
+                lblRuaj.Text = "Ju lutem plotesoni te dhenat!";
+            }
+        }
+
+
+        private void login()
+        {
+            if (txtTestPerdoruesi.Text != null && txtTestFjalekalimi.Text != null)
+            {
+                try
+                {
+
+                    string connectionString = "Server=127.0.0.1; Database=ds_projekti1; UserID=root; Password=805198;";
+
+                    MySqlConnection myDbConn = new MySqlConnection(connectionString);
+
+                    myDbConn.Open();
+
+                    /* Ketu ruhet fjalekalimi i enkriptuar 
+                     * mirepo meqenese ende nuk e kemi implementuar algoritmin vetem per test ja kam vene vleren 'test123'
+                    */
+                    string fjalekalimiEnkriptuar = "test123";
+
+                    string users = "Select * from perdoruesit where username='" + txtTestPerdoruesi.Text + "' and fjalekalimi='" + fjalekalimiEnkriptuar + "'";
+
+                    MySqlCommand command = new MySqlCommand(users, myDbConn);
+
+                    command.ExecuteNonQuery();
+
+                    DataTable rezultatet = new DataTable();
+
+                    MySqlDataAdapter DA = new MySqlDataAdapter(command);
+
+                    DA.Fill(rezultatet);
+
+                    int i = 0;
+                    i = Convert.ToInt32(rezultatet.Rows.Count.ToString());
+
+                    if (i == 0)
+                    {
+                        lblKyqja.Text = "Ju keni dhene Perdoruesi-n apo Fjalekalimi-n jovalid!";
+                    }
+                    else
+                    {
+                        lblKyqja.Text = "Ju jeni kyqur me sukses!";
+                    }
+                    myDbConn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                lblKyqja.Text = "Ju lutem shkruani Perdoruesi-n dhe Fjalekalimi-n!";
+            }
+
+        }
+
+        public static string Enkripto(string fjalekalimi, string salti)
+        {
+            StringBuilder strFjalekalimi = new StringBuilder(fjalekalimi);
+            StringBuilder strSalti = new StringBuilder(salti);
+            StringBuilder enkriptimi = new StringBuilder();
+
+            
+            for (int i = 1; i <= strFjalekalimi.Length; i++)
+            {
+                enkriptimi.Append(strSalti[i%8]);
+                enkriptimi.Append(strFjalekalimi[i%strFjalekalimi.Length]);
+            }
+            return enkriptimi.ToString();
+        }
+
+
+        }
+}
